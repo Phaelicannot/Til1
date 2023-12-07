f85b4519dec86548eba195ded1f47ec06cb967b9
Phaelicannot <phaelica.w@gmail.com>
2023-12-07 22:03:18 +0100

    Fick hjälp

--- enemies.gd
+++ enemies.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends Area2D
 
 
 var speed = 300.0
@@ -8,17 +8,22 @@ var posY = 0
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _process(delta):
-	var enemy_shape = $CollisionShape2D.shape.get_rect()
-	var player_shape = $"../player/CollisionShape2D".shape.get_rect()
-	position.x = posX
-	position.y = posY
-	if enemy_shape.intersects(player_shape):
-		posX = randf_range(10, 100)
-		posY = randf_range(0, 10)
+#func _process(delta):
+#	var enemy_shape = $CollisionShape2D.shape.get_rect()
+#	var player_shape = $"../player/CollisionShape2D".shape.get_rect()
+#	position.x = posX
+#	position.y = posY
+#	if enemy_shape.intersects(player_shape):
+#		posX = randf_range(10, 100)
+#		posY = randf_range(0, 10)
 		
 	#var player = $"../player"
 	#var movement = position - player.position 
 	#var velocity = movement * speed
 	#speed += delta
 	#move_and_slide()
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		position = Vector2(randf_range(-100, 100), randf_range(-100, 0))
